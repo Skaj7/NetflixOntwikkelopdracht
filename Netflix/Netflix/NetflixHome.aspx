55fd42590158d33f8d9d03f779aafe19764394c2
Skaj7 <kaj753@hotmail.com>
2015-06-08 13:40:12 +0200

    dbs connect

--- Netflix/Netflix/NetflixHome.aspx
+++ Netflix/Netflix/NetflixHome.aspx
@@ -19,8 +19,9 @@
             <video id="billboardvid" autoplay>
                 <source src="http://so1.akam.nflximg.com/soa7/433/2085437433.mp4?v=1&amp;e=1433182153&amp;t=1XIATIRc25-vXrlxyDSeqj5yLGw" type="video/mp4" />
             </video>
-
-            <img id="billboardpic" src="extra/Sense8-2.jpg" alt="Alternate Text" />
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+            <%--<img id="billboardpic" src="extra/Sense8-2.jpg" alt="Alternate Text" />--%>
             
         </div>
     </form>
--- Netflix/Netflix/NetflixHome.aspx.cs
+++ Netflix/Netflix/NetflixHome.aspx.cs
@@ -4,17 +4,77 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Oracle.ManagedDataAccess.Client;
+using System.Data.Common;
+using System.Configuration;
 
 namespace Netflix
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
-        private DatabaseQuery dq;
         
         protected void Page_Load(object sender, EventArgs e)
         {
-            dq = new DatabaseQuery();
-            int a = dq.example();
+
+                
+
+
+
+
+            //    con.ConnectionString = ConfigurationManager.ConnectionStrings["OracleConnection"].ConnectionString;
+            //    con.Open();
+            //    DbCommand com = OracleClientFactory.Instance.CreateCommand();
+            //    if (com == null)
+            //    {
+            //        //return "Error! No Command";
+            //    }
+            //    com.Connection = con;
+            //    com.CommandText = "SELECT reservering.betaald FROM polsbandje INNER JOIN reservering_polsbandje ON polsbandje.id = reservering_polsbandje.polsbandje_id INNER JOIN reservering ON reservering_polsbandje.reservering_id = reservering.id WHERE polsbandje.barcode = :1";
+                //AddParameterWithValue(com, "barc", (string)tbBarcode.Text);
+
+                //lblBarcodeObject.Text = tbBarcode.Text;
+                //try
+                //{
+                //    if ((short)com.ExecuteScalar() == 0)
+                //    {
+                //        lblBetaaldobject.Text = "Niet betaald";
+
+                //    }
+                //    else
+                //    {
+                //        lblBetaaldobject.Text = "Betaald";
+                //    }
+                //}
+                //catch (NullReferenceException)
+                //{
+                //    lblBarcodeObject.Text = "Barcode is niet gevonden";
+                //}
+            }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+        using (DbConnection con = OracleClientFactory.Instance.CreateConnection())
+                {
+                    if (con == null)
+                    {
+                        //return "Error! No Connection";
+                    }
+                    con.ConnectionString = ConfigurationManager.ConnectionStrings["OracleConnection"].ConnectionString;
+                    con.Open();
+                    DbCommand com = OracleClientFactory.Instance.CreateCommand();
+                    if (com == null)
+                    {
+                        //return "Error! No Command";
+                    }
+                    com.Connection = con;
+                    com.CommandText = "Select * FROM ACCOUNT_APPARAAT";
+                    //AddParameterWithValue(com, "barc", (string)tbBarcode.Text);
+                    DbDataReader reader = com.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Label1.Text = Label1.Text + reader[2].ToString();
+                    }
+                }
+        }
         }
     }
-}
