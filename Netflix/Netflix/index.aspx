6965837bc2d285e56c13d8d2ac43719f9e6ca873
Skaj7 <kaj753@hotmail.com>
2015-06-13 10:04:03 +0200

    index

---
+++ Netflix/Netflix/index.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Masterpage.Master" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="Netflix.Test" %>
+
+<%@ Register Src="~/video.ascx" TagPrefix="uc1" TagName="video" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link href="Style/Test.css" rel="stylesheet" />
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="content" runat="server">
+    
+        <video id="billboardvid" autoplay>
+             <source src="http://so1.akam.nflximg.com/soa7/433/2085437433.mp4?v=1&amp;e=1433182153&amp;t=1XIATIRc25-vXrlxyDSeqj5yLGw" type="video/mp4" />
+        </video>
+
+    <div id="innerContent" clientidmode="Static" runat="server">
+
+    </div>
+    <a href="http://localhost:10187/video1.aspx">video1</a>
+
+</asp:Content>
---
+++ Netflix/Netflix/index.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Netflix
+{
+    public partial class Test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            for (int i = 1; i <= 3; i++)
+            {
+                LoadVideo(i);
+            }
+        }
+
+        private void LoadVideo(int id)
+        {
+            var con = DbCon.GetOracleConnection();
+            var com = con.CreateCommand();
+            com.CommandText = "SELECT videolink, videoid from video where videoid='"+id+"'";
+
+            var r = com.ExecuteReader();
+
+            while (r.Read())
+            {
+                var uc = (video)Page.LoadControl("~/video.ascx");
+                uc.VideoLink = r["videolink"].ToString();
+                uc.id = r["videoid"].ToString();
+
+                innerContent.Controls.Add(uc);
+                uc.loadData();
+            } 
+        }
+    }
+}
